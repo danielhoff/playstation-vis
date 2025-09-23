17a26c0d4f41cef39c1c4df1e7c7b8ba53a0ac56
Dan <danielhoff20@gmail.com>
2025-09-23 16:35:51 +0100

    UI Library: button creation

---
+++ src/lib/components/ui-library/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    interface Props {
+        label?: string;
+        type?: 'primary' | 'secondary'
+    }
+
+    let {
+        label = '',
+        type = 'primary'
+    }: Props = $props();
+
+</script>
+
+<button>
+    {label}
+</button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../styles/app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Button from '$lib/components/ui-library/Button.svelte';
 
 	let { children } = $props();
 </script>
@@ -12,5 +13,7 @@
 <!-- Dark class goes here :) -->
 <div class="bg-(--background-color) p-(--space-xl) h-dvh w-dvw">
 	<h1 class="font-space-mono text-ps-blue">This appears on all pages</h1>
+	<Button></Button>
+
 	{@render children?.()}
 </div>
