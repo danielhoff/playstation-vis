bdc42e3872288b6d9ebe903f78d07b1d191745ae
Dan <danielhoff20@gmail.com>
2025-09-29 21:39:59 +0100

    d3 install + types + test

---
+++ src/lib/components/LineChart.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import * as d3 from 'd3';
+
+    const xScale = d3.scaleLinear().domain([0, 10]).range([0, 100]);
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
     import { metricsData, groupsData, boundariesFormatted } from '$lib/stores/data.svelte';
+    import Container from '$lib/components/Container.svelte';
+    import LineChart from '$lib/components/LineChart.svelte';
 
     $inspect(metricsData[0]);
     $inspect(groupsData[0]);
     $inspect(boundariesFormatted());
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Container>
+    <LineChart></LineChart>
+</Container>
+
+
