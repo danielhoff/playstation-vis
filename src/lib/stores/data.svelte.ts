a11ff2f10dc04c059ff40036e6d386e20b406bc9
Dan <danielhoff20@gmail.com>
2025-09-29 19:18:40 +0100

    installed d3

--- src/lib/stores/data.svelte.ts
+++ src/lib/stores/data.svelte.ts
@@ -3,6 +3,7 @@ import metricsJson from '$lib/data/metrics.json';
 import groupsJson from '$lib/data/groups.json';
 import type { Boundaries, Metric, Group } from '$lib/types.ts';
 
+// makes all 3 possible to grab the data like metricsData[0] rather than metricsData.metrics[0]
 export const boundariesData = $state<Boundaries>(boundariesJson.boundaries);
 
 export const metricsData = $state<Metric[]>(
